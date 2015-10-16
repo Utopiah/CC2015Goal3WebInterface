4402359b4fd23323bd16dcefd2e00a31f92da908
Fabien Benetou <fabien-services@benetou.fr>
2015-10-16 09:32:49 +0200

    updated models

--- mysite/monthlyinterface/migrations/0001_initial.py
+++ mysite/monthlyinterface/migrations/0001_initial.py
@@ -13,9 +13,26 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Creation',
             fields=[
-                ('id', models.AutoField(primary_key=True, serialize=False, auto_created=True, verbose_name='ID')),
-                ('desire_theme', models.CharField(max_length=200)),
+                ('id', models.AutoField(primary_key=True, auto_created=True, serialize=False, verbose_name='ID')),
+                ('desired_theme', models.CharField(max_length=200, default='unset')),
                 ('pub_date', models.DateTimeField(verbose_name='date created')),
             ],
+        ),
+        migrations.CreateModel(
+            name='Desired_Format',
+            fields=[
+                ('id', models.AutoField(primary_key=True, auto_created=True, serialize=False, verbose_name='ID')),
+                ('name', models.CharField(max_length=200, default='unset')),
+            ],
+        ),
+        migrations.AddField(
+            model_name='creation',
+            name='desired_format',
+            field=models.ForeignKey(to='monthlyinterface.Desired_Format'),
+        ),
+        migrations.AddField(
+            model_name='creation',
+            name='parent',
+            field=models.ForeignKey(to='monthlyinterface.Creation'),
         ),
     ]
--- mysite/monthlyinterface/models.py
+++ mysite/monthlyinterface/models.py
@@ -4,10 +4,14 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
-
+class Desired_Format(models.Model):
+    name = models.CharField(max_length=200, blank=False, default="unset")
+    
 class Creation(models.Model):
-    desired_theme = models.CharField(max_length=200)
+    desired_theme = models.CharField(max_length=200, blank=False, default="unset")
     pub_date = models.DateTimeField('date created')
+    desired_format = models.ForeignKey(Desired_Format)
+    parent = models.ForeignKey('self')
     def __str__(self):
         return self.desired_theme
     def was_published_recently(self):
