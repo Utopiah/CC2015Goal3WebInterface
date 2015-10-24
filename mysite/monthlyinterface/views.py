6515a09132663825efeb7c66c9516b71856d22f5
Fabien Benetou <fabien-services@benetou.fr>
2015-10-24 17:57:14 +0200

    conecting the frontend with the backend

--- mysite/monthlyinterface/views.py
+++ mysite/monthlyinterface/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+import sys
+sys.path.insert(0, '/home/fabien/Prototypes/CC2015Goal3Month1')
 
-# Create your views here.
+#import BlendMeAPicture
+# problem between Python2 and Python3...
+
+def BlenMeAPicture(string):
+    return string
+    #cheating until it becomes a library compatible to python3
+
+def index(request):
+   return HttpResponse("Just testing so far : " + BlenMeAPicture("Brussels"))
+    # make it work outside of main
+
