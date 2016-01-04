39abd993e9b39b1d725030632d4d6ce661008e9c
Fabien Benetou <fabien-services@benetou.fr>
2016-01-04 09:39:14 +0100

    Improper function name, todo added

--- mysite/monthlyinterface/views.py
+++ mysite/monthlyinterface/views.py
@@ -5,11 +5,11 @@ sys.path.insert(0, '/home/fabien/Prototypes/CC2015Goal3Month1')
 #import BlendMeAPicture
 # problem between Python2 and Python3...
 
-def BlenMeAPicture(string):
+def BlendMeAPicture(string):
     return string
-    #cheating until it becomes a library compatible to python3
+    #cheating until it (month1) becomes a library compatible to python3
 
 def index(request):
-   return HttpResponse("Just testing so far : " + BlenMeAPicture("Brussels"))
+   return HttpResponse("Just testing so far : " + BlendMeAPicture("rewrite month1 to blend several pictures based on given topics"))
     # make it work outside of main
 
