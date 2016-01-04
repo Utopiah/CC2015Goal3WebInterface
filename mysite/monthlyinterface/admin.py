559043ea4dcbe1cf3d5a096f22922a63da4051d8
Fabien Benetou <fabien-services@benetou.fr>
2016-01-04 09:35:40 +0100

    Forced unrequired parent to allow for the first instances to exist

--- mysite/monthlyinterface/admin.py
+++ mysite/monthlyinterface/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Creation
+from .models import Desired_Format
 
 admin.site.register(Creation)
+admin.site.register(Desired_Format)
--- mysite/monthlyinterface/models.py
+++ mysite/monthlyinterface/models.py
@@ -11,7 +11,7 @@ class Creation(models.Model):
     desired_theme = models.CharField(max_length=200, blank=False, default="unset")
     pub_date = models.DateTimeField('date created')
     desired_format = models.ForeignKey(Desired_Format)
-    parent = models.ForeignKey('self')
+    parent = models.ForeignKey('self', null=True, blank=True)
     def __str__(self):
         return self.desired_theme
     def was_published_recently(self):
