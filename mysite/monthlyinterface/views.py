41b0874f8cb1f5ac76eb246612ffa7a402e4d14b
Fabien Benetou <fabien-services@benetou.fr>
2016-01-05 09:47:19 +0100

    Redirection and beginning of a view (still no processing)

--- mysite/monthlyinterface/views.py
+++ mysite/monthlyinterface/views.py
@@ -6,6 +6,7 @@ sys.path.insert(0, '/home/fabien/Prototypes/CC2015Goal3Month1')
 # problem between Python2 and Python3...
 
 def BlendMeAPicture(string):
+    # heavy processing should be done at the models side, fat models skinny views
     return string
     #cheating until it (month1) becomes a library compatible to python3
 
@@ -13,3 +14,5 @@ def index(request):
    return HttpResponse("Just testing so far : " + BlendMeAPicture("rewrite month1 to blend several pictures based on given topics"))
     # make it work outside of main
 
+def detail(request, creation_id):
+   return HttpResponse("You are looking at creation %s." % creation_id)
