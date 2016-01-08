0c70b0237f374ab9ed6218d4afddb03bbdb01004
Fabien Benetou <fabien-services@benetou.fr>
2016-01-08 09:31:56 +0100

    Moved the heavy code placeholder to the model

--- mysite/monthlyinterface/models.py
+++ mysite/monthlyinterface/models.py
@@ -6,6 +6,8 @@ from django.utils import timezone
 # Create your models here.
 class Desired_Format(models.Model):
     name = models.CharField(max_length=200, blank=False, default="unset")
+    def __str__(self):
+        return self.name
     
 class Creation(models.Model):
     desired_theme = models.CharField(max_length=200, blank=False, default="unset")
@@ -16,3 +18,11 @@ class Creation(models.Model):
         return self.desired_theme
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    def generate(self):
+        return "A blend between all or at least most of the themes " + self.desired_theme + " for the specified format "
+        # fails for implicit to str " for the specified format (" + self.desired_format + ")"
+
+        #import BlendMeAPicture
+        # problem between Python2 and Python3...
+        #cheating until it (month1) becomes a library compatible to python3
+        # https://github.com/Utopiah/CC2015Goal3Month1/blob/master/BlendMeAPicture.py
--- mysite/monthlyinterface/views.py
+++ mysite/monthlyinterface/views.py
@@ -5,13 +5,6 @@ sys.path.insert(0, '/home/fabien/Prototypes/CC2015Goal3Month1')
 
 from .models import Creation
 
-def BlendMeAPicture(string):
-#import BlendMeAPicture
-# problem between Python2 and Python3...
-# NO, heavy processing should be done at the models side, fat models skinny views
-    return "blended the images of "+string+ " topic together"
-    #cheating until it (month1) becomes a library compatible to python3
-
 def index(request):
     latest_creation_list = Creation.objects.order_by('-pub_date')[:5]
     template = loader.get_template('monthlyinterface/index.html')
@@ -19,7 +12,7 @@ def index(request):
         'latest_creation_list': latest_creation_list
     }
     return HttpResponse(template.render(context, request))
-    #return HttpResponse(goal  +  "<hr>" + "Expected result : " + BlendMeAPicture("no topic provided!"))
 
 def detail(request, creation_id):
-   return HttpResponse("(UI exploration https://wireframe.cc/74FKag ) You are looking at creation %s.<br>Fork it button." % creation_id)
+    return HttpResponse(Creation.objects.get(pk=creation_id).generate())
+    return HttpResponse("(UI exploration https://wireframe.cc/74FKag ) You are looking at creation %s.<br>Fork it button." % creation_id)
